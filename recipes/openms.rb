5b96ce30e13fab1b9b0a9b5e571d96cab6e5d213
Jörgen Brandt <joergen.brandt@onlinehome.de>
2015-09-23 13:54:04 +0200

    OpenMS now works.

--- recipes/openms.rb
+++ recipes/openms.rb
@@ -4,20 +4,20 @@
 #
 # Copyright (c) 2015 Jörgen Brandt, All Rights Reserved.
 
-openms_dir = "#{node.dir.software}/OpenMS-2.0.0"
-openms_targz = "#{node.dir.archive}/OpenMS-2.0.0_src.tar.gz"
-openms_link = "http://downloads.sourceforge.net/project/open-ms/OpenMS/OpenMS-2.0/OpenMS-2.0.0_src.tar.gz"
 
-contrib_dir = "#{openms_dir}/contrib"
-build_dir = "#{openms_dir}/OpenMS-build"
+openms_src_dir = "#{node.dir.software}/OpenMS/OpenMS-src"
+openms_build_dir = "#{node.dir.software}/OpenMS/OpenMS-build"
+
+contrib_src_dir = "#{node.dir.software}/OpenMS/contrib-src"
+contrib_build_dir = "#{node.dir.software}/OpenMS/contrib-build"
 
 package "cmake"
 package "g++"
 package "autoconf"
+package "zlib1g-dev"
 package "qt4-dev-tools"
 package "patch"
 package "libtool"
-package "make"
 package "git"
 package "libboost-regex-dev"
 package "libboost-iostreams-dev"
@@ -26,49 +26,64 @@ package "libboost-math-dev"
 package "libsvm-dev"
 package "libglpk-dev"
 package "libzip-dev"
-package "zlib1g-dev"
 package "libxerces-c-dev"
 package "libbz2-dev"
+package "libeigen3-dev"
 
-directory node.dir.archive
 directory node.dir.software
+directory "#{node.dir.software}/OpenMS"
+directory openms_build_dir
+directory contrib_build_dir
 
-# fetch from github
-# git "git_clone_openms" do
-#   action :checkout
-#   repository "https://github.com/OpenMS/OpenMS.git"
-#   destination openms_dir
-# end
-
-# fetch package
-remote_file openms_targz do
-  action :create_if_missing
-  source openms_link
+# fetch OpenMS source code
+git "git_clone_openms_src" do
+  action :checkout
+  repository "https://github.com/OpenMS/OpenMS.git"
+  revision "4776889fe40e1b001407b50b9a3de2fd1c175515"
+  destination openms_src_dir
 end
 
-# extract package
-bash "extract_openms" do
-  code "tar xf #{openms_targz} -C #{node.dir.software}"
-  not_if "#{Dir.exists?( openms_dir )}"
+
+# fetch OpenMS contrib
+git "git_clone_openms_contrib" do
+  action :checkout
+  repository "https://github.com/OpenMS/contrib.git"
+  revision "f6b44d770b6c1b23972a8891753472e60a356253"
+  destination contrib_src_dir
 end
 
-# build zlib
-bash "build_openms_contrib_zlib" do
-  code "sudo cmake -DBUILD_TYPE=ZLIB ."
-  cwd contrib_dir
+
+# build seqan
+bash "build_openms_contrib_seqan" do
+  code "cmake -DBUILD_TYPE=SEQAN #{contrib_src_dir}"
+  cwd contrib_build_dir
+  not_if "#{Dir.exists?( "#{contrib_build_dir}/src/seqan-1.4.1" )}"
 end
 
-# build contrib
-bash "build_openms_contrib_all" do
-  code "sudo cmake -DBUILD_TYPE=ALL ."
-  cwd contrib_dir
+# build wildmagic
+bash "build_openms_contrib_wildmagic" do
+  code "cmake -DBUILD_TYPE=WILDMAGIC #{contrib_src_dir}"
+  cwd contrib_build_dir
+  not_if "#{Dir.exists?( "#{contrib_build_dir}/src/WildMagic5" )}"
+end
+
+# configure openms
+bash "configure_openms" do
+  code <<-SCRIPT
+cmake -DCMAKE_PREFIX_PATH="#{contrib_build_dir};/usr;/usr/local" -DBOOST_USE_STATIC=OFF #{openms_src_dir}
+  SCRIPT
+  cwd openms_build_dir
+  not_if "#{File.exists?( "#{openms_build_dir}/Makefile" )}"
 end
 
 # build openms
-#directory build_dir
-#bash "build_openms" do
-#  code <<-SCRIPT
-#cmake -DCMAKE_PREFIX_PATH="#{contrib_dir};/usr;/usr/local" -DBOOST_USE_STATIC=OFF ..
-#  SCRIPT
-#  cwd build_dir
-#end
+bash "build_openms" do
+  code "make"
+  cwd openms_build_dir
+  not_if "#{File.exists?( "#{openms_build_dir}/lib/libOpenMS.so" )}"
+end
+
+bash "update_environment" do
+  code "echo export LD_LIBRARY_PATH=#{openms_build_dir}/lib >> /etc/environment"
+  not_if "grep LD_LIBRARY_PATH /etc/environment"
+end
