b43207f58c262a3a4e047f898a4f61c0d40af7bc
Jörgen Brandt <joergen.brandt@onlinehome.de>
2015-10-13 13:32:13 +0200

    OMSSA mods.xml and usermods.xml put into standard location.

--- attributes/default.rb
+++ attributes/default.rb
@@ -3,6 +3,7 @@ default.dir.software = "/opt/software"
 default.dir.archive  = "/opt/archive"
 default.dir.jar      = "#{default.dir.software}/jar"
 default.dir.data     = "/opt/data"
+default.dir.etc      = "/usr/local/etc"
 
 # directories existent by default
 default.dir.bin      = "/usr/local/bin"
