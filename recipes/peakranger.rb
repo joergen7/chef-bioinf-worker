0b8466f3312b035193905621b3030283f513f990
Jörgen Brandt <joergen.brandt@onlinehome.de>
2015-10-13 11:33:36 +0200

    Peakranger updated.

--- recipes/peakranger.rb
+++ recipes/peakranger.rb
@@ -4,11 +4,6 @@
 #
 # Copyright (c) 2015 Jörgen Brandt, All Rights Reserved.
 
-peakranger_link = "http://downloads.sourceforge.net/project/ranger/PeakRanger-1.14-Linux-x86_64.zip"
-peakranger_zip  = "#{node.dir.archive}/#{File.basename( peakranger_link )}"
-peakranger_dir  = "#{node.dir.software}/PeakRanger-1.14-Linux-x86_64"
-
-
 
 
 directory node.dir.software
@@ -16,17 +11,53 @@ directory node.dir.archive
 
 package "unzip"
 
-remote_file peakranger_zip do
+
+
+
+
+
+
+
+
+
+peakranger114_link = "http://downloads.sourceforge.net/project/ranger/PeakRanger-1.14-Linux-x86_64.zip"
+peakranger114_zip  = "#{node.dir.archive}/#{File.basename( peakranger114_link )}"
+peakranger114_dir  = "#{node.dir.software}/PeakRanger-1.14"
+
+remote_file peakranger114_zip do
   action :create_if_missing
-  source peakranger_link
+  source peakranger114_link
   retries 1
 end
 
-bash "extract_peakranger" do
-  code "unzip -o #{peakranger_zip} -d #{peakranger_dir}"
-  not_if "#{Dir.exists?( peakranger_dir )}"
+bash "extract_peakranger_1.14" do
+  code "unzip -o #{peakranger114_zip} -d #{peakranger114_dir}"
+  not_if "#{Dir.exists?( peakranger114_dir )}"
 end
 
 link "#{node.dir.bin}/ranger" do
-  to "#{peakranger_dir}/ranger"
+  to "#{peakranger114_dir}/ranger"
+end
+
+
+
+
+
+peakranger118_link = "http://downloads.sourceforge.net/project/ranger/PeakRanger-1.18-Linux-x86_64.zip"
+peakranger118_zip  = "#{node.dir.archive}/#{File.basename( peakranger118_link )}"
+peakranger118_dir  = "#{node.dir.software}/PeakRanger-1.18"
+
+remote_file peakranger118_zip do
+  action :create_if_missing
+  source peakranger118_link
+  retries 1
+end
+
+bash "extract_peakranger_1.18" do
+  code "unzip -o #{peakranger118_zip} -d #{peakranger118_dir}"
+  not_if "#{Dir.exists?( peakranger118_dir )}"
+end
+
+link "#{node.dir.bin}/peakranger" do
+  to "#{peakranger118_dir}/bin/peakranger"
 end
