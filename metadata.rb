7ea4569f6566c32454e704f00fda8ef41cfb7308
Jorgen Brandt <joergen.brandt@onlinehome.de>
2015-10-16 20:13:43 +0200

    Metadata specified.

--- metadata.rb
+++ metadata.rb
@@ -1,6 +1,6 @@
 name             'chef-bioinf-worker'
-maintainer       'The Authors'
-maintainer_email 'you@example.com'
+maintainer       "Jörgen Brandt"
+maintainer_email "brandjoe@hu-berlin.de"
 license          'all_rights'
 description      'Installs/Configures chef-bioinf-worker'
 long_description 'Installs/Configures chef-bioinf-worker'
