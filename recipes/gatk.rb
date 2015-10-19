aac72636db4ca68cdc30aa08466bf9fca985d2da
Jörgen Brandt <joergen.brandt@onlinehome.de>
2015-10-19 13:39:17 +0200

    Take on GATK.

---
+++ recipes/gatk.rb
@@ -0,0 +1,33 @@
+#
+# Cookbook Name:: chef-bioinf-worker
+# Recipe:: gatk
+#
+# Copyright (c) 2015 Jörgen Brandt, All Rights Reserved.
+
+gatk_github_link = "https://github.com/broadgsa/gatk-protected.git"
+gatk_vsn = "3.4"
+gatk_dir = "#{node.dir.software}/gatk-protected"
+
+include_recipe "chef-bioinf-worker::java"
+
+package "git"
+package "maven"
+
+directory node.dir.software
+
+git "git_clone_openms_src" do
+  action :checkout
+  repository gatk_github_link
+  revision gatk_vsn
+  destination gatk_dir
+  retries 1
+end
+
+
+# TODO: http://gatkforums.broadinstitute.org/discussion/4023/setting-up-your-dev-environment-maven-and-intellij-for-gatk-3
+
+bash "build_gatk" do
+  code "mvn clean package"
+  cwd gatk_dir
+  not_if ""
+end
