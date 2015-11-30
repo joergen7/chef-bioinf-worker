853f4e41d28463f92f14d3b20c256e184e220e9c
Jörgen Brandt <joergen.brandt@onlinehome.de>
2015-11-30 13:49:55 +0100

    Picard tools added.

---
+++ recipes/picard_tools.rb
@@ -0,0 +1,37 @@
+# coding: utf-8
+#
+# Cookbook Name:: chef-bioinf-worker
+# Recipe:: picard_tools
+#
+# Copyright (c) 2015 Jörgen Brandt, All Rights Reserved.
+
+picard_link = "https://github.com/broadinstitute/picard/releases/download/1.141/picard-tools-1.141.zip"
+picard_zip = "#{node.dir.archive}/#{File.basename( picard_link )}"
+picard_dir = "#{node.dir.software}/picard-tools-1.141"
+
+include_recipe "chef-bioinf-worker::java"
+
+package "unzip"
+
+directory node.dir.archive
+directory node.dir.software
+
+remote_file picard_zip do
+  action :create_if_missing
+  source picard_link
+  retries 1
+end
+
+bash "extract_picard_tools" do
+  code "unzip -o #{picard_zip} -d #{node.dir.software}"
+  not_if "#{Dir.exists?( picard_dir )}"
+end
+
+file "#{node.dir.bin}/picard" do
+  content <<-CONTENT
+#!/usr/bin/env bash
+java -jar #{picard_dir}/picard.jar $@  
+  CONTENT
+  mode '0755'
+end
+
