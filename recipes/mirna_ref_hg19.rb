b3fbbe51e2fbd5d235d4aedb6e7ac7df58413ab4
Jörgen Brandt <joergen.brandt@onlinehome.de>
2016-01-18 11:25:44 +0100

    MTI added to miRNA reference.

--- recipes/mirna_ref_hg19.rb
+++ recipes/mirna_ref_hg19.rb
@@ -5,6 +5,7 @@
 # Copyright (c) 2016 Jörgen Brandt, All Rights Reserved.
 
 ref_dir = "#{node.dir.data}/mirna_ref_hg19"
+mti_url = "http://mirtarbase.mbc.nctu.edu.tw/cache/download/6.1/hsa_MTI.xlsx"
 annot_url = "ftp://ftp.sanger.ac.uk/pub/gencode/Gencode_human/release_19/gencode.v19.annotation.gtf.gz"
 ref_vsn = "21"
 id_list = ["hairpin.fa.gz",
@@ -30,5 +31,11 @@ id_list.each { |id|
 remote_file "#{ref_dir}/#{File.basename( annot_url )}" do
   action :create_if_missing
   source annot_url
+  retries 1
+end
+
+remote_file "#{ref_dir}/#{File.basename( mti_url )}" do
+  action :create_if_missing
+  source mti_url
   retries 1
 end
