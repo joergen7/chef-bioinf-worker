c5dde125c98f916768563be24f6612c68dc847dd
Jörgen Brandt <joergen.brandt@onlinehome.de>
2016-01-18 10:32:27 +0100

    miRNA reference files added.

---
+++ recipes/mirna_ref_hg19.rb
@@ -0,0 +1,34 @@
+#
+# Cookbook Name:: chef-bioinf-worker
+# Recipe:: mirna_ref_hg19
+#
+# Copyright (c) 2016 Jörgen Brandt, All Rights Reserved.
+
+ref_dir = "#{node.dir.data}/mirna_ref_hg19"
+annot_url = "ftp://ftp.sanger.ac.uk/pub/gencode/Gencode_human/release_19/gencode.v19.annotation.gtf.gz"
+ref_vsn = "21"
+id_list = ["hairpin.fa.gz",
+           "mature.fa.gz",
+           "genomes/hsa.gff3"
+          ]
+
+directory node.dir.data
+directory ref_dir
+
+id_list.each { |id|
+	
+  url = "ftp://mirbase.org/pub/mirbase/#{ref_vsn}/#{id}"
+
+  remote_file "#{ref_dir}/#{File.basename( url )}" do
+    action :create_if_missing
+    source url
+    retries 1
+  end
+
+}
+
+remote_file "#{ref_dir}/#{File.basename( annot_url )}" do
+  action :create_if_missing
+  source annot_url
+  retries 1
+end
