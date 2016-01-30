d4980c95888d97333433136a4f1b9cfca8d5b581
Jörgen Brandt <joergen.brandt@onlinehome.de>
2016-01-30 16:22:54 +0100

    tabix added to SAMtools recipe.

--- recipes/samtools.rb
+++ recipes/samtools.rb
@@ -12,6 +12,8 @@ samtools_link = "http://downloads.sourceforge.net/project/samtools/samtools/#{sa
 samtools_tar  = "#{node.dir.archive}/#{File.basename( samtools_link )}"
 samtools_dir  = "#{node.dir.software}/samtools-#{samtools_vsn}"
 
+htslib_dir = "#{samtools_dir}/htslib-#{samtools_vsn}"
+
 
 directory node.dir.archive
 directory node.dir.software
@@ -30,12 +32,34 @@ bash "extract_samtools" do
   not_if "#{Dir.exists?( samtools_dir )}"
 end
 
+bash "configure_samtools" do
+  code "./configure"
+  cwd samtools_dir
+  not_if "#{File.exists?( "#{samtools_dir}/config.status" )}"
+end
+
 bash "compile_samtools" do
   code "make"
   cwd samtools_dir
   not_if "#{File.exists?( "#{samtools_dir}/samtools" )}"
 end
 
+bash "configure_htslib" do
+  code "./configure"
+  cwd htslib_dir
+  not_if "#{File.exists?( "#{htslib_dir}/config.status" )}"
+end
+
+bash "compile_htslib" do
+  code "make"
+  cwd htslib_dir
+  not_if "#{File.exists?( "#{htslib_dir}/tabix" )}"
+end
+
 link "#{node.dir.bin}/samtools" do
   to "#{samtools_dir}/samtools"
+end
+
+link "#{node.dir.bin}/tabix" do
+  to "#{htslib_dir}/tabix"
 end
