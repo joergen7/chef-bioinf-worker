03ad45fb1195d87226fc59ee242b14a208ecb4b6
Jörgen Brandt <joergen.brandt@onlinehome.de>
2016-07-02 14:31:17 +0200

    Clustalw and Phylip added.

---
+++ recipes/clustalw.rb
@@ -0,0 +1,7 @@
+#
+# Cookbook Name:: chef-bioinf-worker
+# Recipe:: clustalw
+#
+# Copyright (c) 2016 Jörgen Brandt, All Rights Reserved.
+
+package "clustalw"
---
+++ recipes/phylip.rb
@@ -0,0 +1,7 @@
+#
+# Cookbook Name:: chef-bioinf-worker
+# Recipe:: phylip
+#
+# Copyright (c) 2016 Jörgen Brandt, All Rights Reserved.
+
+package "phylip"
