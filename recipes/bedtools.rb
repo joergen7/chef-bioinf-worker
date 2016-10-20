fd36a6d6402ab02464531f2a688bd94a41192c41
Jörgen Brandt <joergen.brandt@onlinehome.de>
2016-10-20 13:48:31 +0200

    Bedtools recipe updated and warnings removed.

--- recipes/bedtools.rb
+++ recipes/bedtools.rb
@@ -5,16 +5,17 @@
 # Copyright (c) 2015 The Authors, All Rights Reserved.
 
 
-bedtools_link = "https://github.com/arq5x/bedtools2/releases/download/v2.25.0/bedtools-2.25.0.tar.gz"
-bedtools_tar  = "#{node.dir.archive}/#{File.basename( bedtools_link )}"
-bedtools_dir  = "#{node.dir.software}/bedtools2"
+bedtools_vsn  = "2.26.0"
+bedtools_link = "https://github.com/arq5x/bedtools2/releases/download/v#{bedtools_vsn}/bedtools-#{bedtools_vsn}.tar.gz"
+bedtools_tar  = "#{node["dir"]["archive"]}/#{File.basename( bedtools_link )}"
+bedtools_dir  = "#{node["dir"]["software"]}/bedtools2"
 
 
 
 package "g++"
 
-directory node.dir.software
-directory node.dir.archive
+directory node["dir"]["software"]
+directory node["dir"]["archive"]
 
 remote_file bedtools_tar do
   action :create_if_missing
@@ -23,7 +24,7 @@ remote_file bedtools_tar do
 end
 
 bash "extract_bedtools" do
-  code "tar xf #{bedtools_tar} -C #{node.dir.software}"
+  code "tar xf #{bedtools_tar} -C #{node["dir"]["software"]}"
   not_if "#{Dir.exists?( bedtools_dir )}"
 end
 
@@ -33,10 +34,10 @@ bash "compile_bedtools" do
   not_if "#{File.exists?( "#{bedtools_dir}/bin/bedtools" )}"
 end
 
-link "#{node.dir.bin}/sortBed" do
+link "#{node["dir"]["bin"]}/sortBed" do
   to "#{bedtools_dir}/bin/sortBed"
 end
 
-link "#{node.dir.bin}/bedtools" do
+link "#{node["dir"]["bin"]}/bedtools" do
   to "#{bedtools_dir}/bin/bedtools"
 end
