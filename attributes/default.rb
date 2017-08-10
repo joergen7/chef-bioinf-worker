284acfa4fa1595554a0a9a6cf9e0691daed592b0
Jorgen Brandt <joergen.brandt@onlinehome.de>
2017-08-10 14:00:36 +0200

    HG38 removed.

--- attributes/default.rb
+++ attributes/default.rb
@@ -47,34 +47,5 @@ default["hg19"]["idlarge"] = [
 default["hg19"]["smallname"] = "small.fa"
 default["hg19"]["fullname"] = "hg19.tar"
 
-# HG38
-default["hg38"]["dirname"] = "hg38"
-default["hg38"]["idlarge"] = [
-"chr1",
-"chr2",
-"chr3",
-"chr4",
-"chr5",
-"chr6",
-"chr7",
-"chr8",
-"chr9",
-"chr10",
-"chr11",
-"chr12",
-"chr13",
-"chr14",
-"chr15",
-"chr16",
-"chr17",
-"chr18",
-"chr19",
-"chr20",
-"chr21",
-"chr22",
-"chrX",
-"chrY"
-]
-default["hg38"]["smallname"] = "small.fa"
-default["hg38"]["fullname"] = "hg38.tar"
+
 
