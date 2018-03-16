826b82235d172c881b630d097432f2a252f62439
Jörgen <joergen.brandt@onlinehome.de>
2018-03-16 12:43:38 +0100

    Metadata updated.

--- README.md
+++ README.md
@@ -71,3 +71,11 @@ converge the VM.
     cd ..
     sudo chef-client -z -r "chef-bioinf-worker::default"
     
+
+## Authors
+
+- Jörgen Brandt ([@joergen7](https://github.com/joergen7/)) [joergen.brandt@onlinehome.de](mailto:joergen.brandt@onlinehome.de)
+
+## License
+
+[Apache 2.0](https://www.apache.org/licenses/LICENSE-2.0.html)
--- metadata.rb
+++ metadata.rb
@@ -1,7 +1,7 @@
 name             'chef-bioinf-worker'
 maintainer       "Jörgen Brandt"
-maintainer_email "brandjoe@hu-berlin.de"
-license          'all_rights'
+maintainer_email "joergen.brandt@onlinehome.de"
+license          'Apache 2.0'
 description      'Installs/Configures chef-bioinf-worker'
 long_description 'Installs/Configures chef-bioinf-worker'
 version          '0.1.0'
